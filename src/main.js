a1d1bf52b047e752c94cd3d42e296772d2fd1382
Jonas Bird <jonas.bird.m@gmail.com>
2022-05-03 13:35:10 -0400

    feat: add images to thumbBar

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const allImages = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
-
+for (let i = 0; i < allImages.length; i++){
+  let newPath = "./images/" + allImages[i];
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', newPath);
+  thumbBar.appendChild(newImage);
+}
 /* Wiring up the Darken/Lighten button */
