7525cb119e1a01b11bcd53619507971e4894ab93
Jonas Bird <jonas.bird.m@gmail.com>
2022-05-03 14:59:40 -0400

    feat: add toggle to darken/lighten images

--- src/main.js
+++ src/main.js
@@ -21,3 +21,14 @@ thumbBar.addEventListener('click', e => {
 })
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', e => {
+  if (btn.getAttribute("class") == "dark"){
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+  } else {
+    btn.setAttribute("class", "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
+  }
+})
